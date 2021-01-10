bc3095199d20a1b322a8fd7809731beeab86f1d0
cx20 <y-shimano@hondarer-soft.com>
2021-01-10 14:39:19 +0900

    Add DirectX C Language Sampe

---
+++ c/directx9/hello.c
@@ -0,0 +1,220 @@
+#include <windows.h>
+#include <tchar.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+LPDIRECT3D9         g_pD3D       = NULL;
+LPDIRECT3DDEVICE9   g_pd3dDevice = NULL;
+LPD3DXFONT          g_pd3dFont   = NULL;
+RECT                g_rect       = { 0, 0, 0, 0 };
+
+HRESULT InitD3D( HWND hWnd );
+HRESULT InitFont();
+VOID Cleanup();
+VOID Render();
+
+HRESULT InitD3D( HWND hWnd )
+{
+    HRESULT hr;
+    D3DPRESENT_PARAMETERS d3dpp;
+    g_pD3D = Direct3DCreate9( D3D_SDK_VERSION );
+    if( g_pD3D == NULL )
+    {
+        return E_FAIL;
+    }
+
+    d3dpp.BackBufferWidth             = 0;
+    d3dpp.BackBufferHeight            = 0;
+    d3dpp.BackBufferFormat            = D3DFMT_UNKNOWN;
+    d3dpp.BackBufferCount             = 0;
+    d3dpp.MultiSampleType             = D3DMULTISAMPLE_NONE;
+    d3dpp.MultiSampleQuality          = 0;
+    d3dpp.SwapEffect                  = D3DSWAPEFFECT_DISCARD;
+    d3dpp.hDeviceWindow               = NULL;
+    d3dpp.Windowed                    = TRUE;
+    d3dpp.EnableAutoDepthStencil      = 0;
+    d3dpp.AutoDepthStencilFormat      = D3DFMT_UNKNOWN;
+    d3dpp.Flags                       = 0;
+    d3dpp.FullScreen_RefreshRateInHz  = 0;
+    d3dpp.PresentationInterval        = 0;
+
+    hr = g_pD3D->lpVtbl->CreateDevice(
+        g_pD3D,
+        D3DADAPTER_DEFAULT,
+        D3DDEVTYPE_HAL,
+        hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &d3dpp,
+        &g_pd3dDevice
+    );
+    
+    if( FAILED( hr ) )
+    {
+        return E_FAIL;
+    }
+
+    return S_OK;
+}
+
+HRESULT InitFont()
+{
+    HRESULT hr;
+    
+    D3DXFONT_DESC lf;
+    lf.Height          = 16;
+    lf.Width           = 0;
+    lf.Weight          = 0;
+    lf.MipLevels       = 1;
+    lf.Italic          = 0;
+    lf.CharSet         = SHIFTJIS_CHARSET;
+    lf.OutputPrecision = OUT_TT_ONLY_PRECIS;
+    lf.Quality         = PROOF_QUALITY;
+    lf.PitchAndFamily  = FIXED_PITCH | FF_MODERN;
+    lstrcpy( lf.FaceName, _T("‚l‚r ƒSƒVƒbƒN") );
+
+    hr = D3DXCreateFontIndirect( g_pd3dDevice, &lf, &g_pd3dFont );
+    if ( FAILED( hr ) )
+    {
+        Cleanup();
+        return hr;
+    }
+
+    hr = g_pd3dFont->lpVtbl->DrawText(
+        g_pd3dFont,
+        NULL, 
+        _T("Hello, DirectX World!"),
+        -1,
+        &g_rect,
+        DT_CALCRECT | DT_LEFT | DT_SINGLELINE,
+        0xffffffff
+    );
+
+    if ( FAILED( hr ) )
+    {
+        Cleanup();
+        return hr;
+    }
+
+    return hr;
+}
+
+VOID Cleanup()
+{
+    if ( g_pd3dFont != NULL )
+    {
+        g_pd3dFont->lpVtbl->Release( g_pd3dFont );
+    }
+
+    if( g_pd3dDevice != NULL )
+    {
+        g_pd3dDevice->lpVtbl->Release( g_pd3dDevice );
+    }
+
+    if( g_pD3D != NULL )
+    {
+        g_pD3D->lpVtbl->Release( g_pD3D );
+    }
+}
+
+VOID Render()
+{
+    if( g_pd3dDevice == NULL )
+    {
+        return;
+    }
+
+    if ( g_pd3dFont == NULL )
+    {
+        return;
+    }
+
+    g_pd3dDevice->lpVtbl->Clear(
+        g_pd3dDevice,
+        0, 
+        NULL,
+        D3DCLEAR_TARGET,
+        D3DCOLOR_XRGB( 0, 0, 255 ),
+        1.0f,
+        0
+    );
+
+    if( SUCCEEDED( g_pd3dDevice->lpVtbl->BeginScene( g_pd3dDevice ) ) )
+    {
+        g_pd3dFont->lpVtbl->DrawText( 
+            g_pd3dFont,
+            NULL,
+            _T("Hello, DirectX World!"),
+            -1,
+            &g_rect,
+            DT_LEFT | DT_SINGLELINE,
+            0xffffffff
+        );
+
+        g_pd3dDevice->lpVtbl->EndScene( g_pd3dDevice );
+    }
+
+    g_pd3dDevice->lpVtbl->Present( g_pd3dDevice, NULL, NULL, NULL, NULL );
+}
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    switch( message )
+    {
+        case WM_DESTROY:
+            Cleanup();
+            PostQuitMessage( 0 );
+            return 0;
+
+        case WM_PAINT:
+            Render();
+            ValidateRect( hWnd, NULL );
+            return 0;
+    }
+
+    return DefWindowProc( hWnd, message, wParam, lParam );
+}
+
+int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
+{
+    LPCTSTR lpszClassName = _T("helloWindow");
+    LPCTSTR lpszWindowName = _T("Hello, World!");
+    MSG msg;
+    HWND hWnd;
+
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = lpszClassName;
+    wcex.hIconSm        = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+    RegisterClassEx(&wcex);
+    hWnd = CreateWindow(
+        lpszClassName,
+        lpszWindowName,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+        NULL, NULL, hInstance, NULL
+    );
+
+    InitD3D( hWnd );
+    InitFont();
+
+    ShowWindow( hWnd, SW_SHOWDEFAULT );
+    UpdateWindow( hWnd );
+
+    while( GetMessage( &msg, NULL, 0, 0 ) )
+    {
+        TranslateMessage( &msg );
+        DispatchMessage( &msg );
+    }
+
+    return 0;
+}
---
+++ c/directx9/readme.txt
@@ -0,0 +1,29 @@
+compile:
+
+C:\> SET DXSDK_DIR=C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)
+C:\> SET INCLUDE=%DXSDK_DIR%\INCLUDE;%INCLUDE%
+C:\> SET LIB=%DXSDK_DIR%\Lib\x86;%LIB%
+
+C:\> cl hello.c ^
+         /link ^
+         user32.lib ^
+         dxguid.lib ^
+         d3d9.lib ^
+         d3dx9.lib ^
+         /SUBSYSTEM:WINDOWS
+
+Result:
++------------------------------------------+
+|Hello, World!                    [_][~][X]|
++------------------------------------------+
+|Hello, DirectX World!                     |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
+|                                          |
++------------------------------------------+
