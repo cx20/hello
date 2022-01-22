2c6610a41d71e4ec7806d6da6d878f9a071d8054
cx20 <y-shimano@hondarer-soft.com>
2022-01-22 14:31:57 +0900

    Added DirectX9 triangle sample in C language

---
+++ c/directx9/triangle/build.bat
@@ -0,0 +1,9 @@
+SET DXSDK_DIR=C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)
+SET INCLUDE=%DXSDK_DIR%\INCLUDE;%INCLUDE%
+SET LIB=%DXSDK_DIR%\Lib\x86;%LIB%
+
+cl hello.c ^
+         /link ^
+         user32.lib ^
+         d3d9.lib ^
+         /SUBSYSTEM:WINDOWS
