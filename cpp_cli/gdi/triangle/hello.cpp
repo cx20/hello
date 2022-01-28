f13b4206d3798e02a6207579f9419481c5158332
cx20 <y-shimano@hondarer-soft.com>
2022-01-28 12:53:32 +0900

    Added GDI triangle sample in C++/CLI

---
+++ cpp_cli/gdi/triangle/hello.cpp
@@ -0,0 +1,132 @@
+#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void OnPaint(HDC hdc);
+void DrawTriangle(HDC hdc);
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wcex;
+    HWND hwnd;
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
+    BOOL bQuit = FALSE;
+
+    wcex.cbSize         = sizeof(WNDCLASSEX);
+    wcex.style          = CS_OWNDC;
+    wcex.lpfnWndProc    = WindowProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = _T("WindowClass");
+    wcex.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    hwnd = CreateWindowEx(0,
+                          _T("WindowClass"),
+                          _T("Hello, World!"),
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          640,
+                          480,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+
+    while (!bQuit)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+    }
+
+    DestroyWindow(hwnd);
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    switch (uMsg)
+    {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+        break;
+
+        case WM_DESTROY:
+            return 0;
+
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            OnPaint(hdc);
+            EndPaint(hWnd, &ps);
+            break;
+         default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+void OnPaint(HDC hdc)
+{
+    DrawTriangle(hdc);
+}
+
+void DrawTriangle(HDC hdc)
+{
+    int WIDTH  = 640;
+    int HEIGHT = 480;
+
+    TRIVERTEX vertex[3];
+    vertex[0].x     = WIDTH  * 1 / 2;
+    vertex[0].y     = HEIGHT * 1 / 4;
+    vertex[0].Red   = 0xffff;
+    vertex[0].Green = 0x0000;
+    vertex[0].Blue  = 0x0000;
+    vertex[0].Alpha = 0x0000;
+
+    vertex[1].x     = WIDTH  * 3 / 4;
+    vertex[1].y     = HEIGHT * 3 / 4;
+    vertex[1].Red   = 0x0000;
+    vertex[1].Green = 0xffff;
+    vertex[1].Blue  = 0x0000;
+    vertex[1].Alpha = 0x0000;
+
+    vertex[2].x     = WIDTH  * 1 / 4;
+    vertex[2].y     = HEIGHT * 3 / 4; 
+    vertex[2].Red   = 0x0000;
+    vertex[2].Green = 0x0000;
+    vertex[2].Blue  = 0xffff;
+    vertex[2].Alpha = 0x0000;
+
+    GRADIENT_TRIANGLE gTriangle;
+    gTriangle.Vertex1 = 0;
+    gTriangle.Vertex2 = 1;
+    gTriangle.Vertex3 = 2;
+
+    GradientFill(hdc, vertex, 3, &gTriangle, 1, GRADIENT_FILL_TRIANGLE);
+}
