806383fdfa827761254914be1a8cee454d64f1af
cx20 <y-shimano@hondarer-soft.com>
2022-02-06 15:08:52 +0900

    Added WebGL1/2 triangle samples in C

---
+++ wasm_c/webgl2/triangle/hello.c
@@ -0,0 +1,92 @@
+// File : hello.cpp
+// Compile : emcc hello.c -s WASM=1 -s MIN_WEBGL_VERSION=2 -s MAX_WEBGL_VERSION=2 -O3 -o index.js
+
+#include <stdio.h>
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#include <GLES2/gl2.h>
+
+// Shader sources
+const GLchar* vertexSource =
+    "#version 300 es                              \n"
+    "in  vec3 position;                           \n"
+    "in  vec3 color;                              \n"
+    "out vec4 vColor;                             \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  vColor = vec4(color, 1.0);                 \n"
+    "  gl_Position = vec4(position, 1.0);         \n"
+    "}                                            \n";
+const GLchar* fragmentSource =
+    "#version 300 es                              \n"
+    "precision mediump float;                     \n"
+    "in  vec4 vColor;                             \n"
+    "out vec4 outColor;                           \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  outColor = vColor;                         \n"
+    "}                                            \n";
+
+int main()
+{
+    EmscriptenWebGLContextAttributes attr;
+    emscripten_webgl_init_context_attributes(&attr);
+
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_create_context("#canvas", &attr);
+    emscripten_webgl_make_context_current(ctx);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo[2];
+    glGenBuffers(2, vbo);
+
+    GLfloat vertices[] = {
+          0.0f,  0.5f, 0.0f,
+          0.5f, -0.5f, 0.0f,
+         -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] = {
+         1.0f,  0.0f,  0.0f,
+         0.0f,  1.0f,  0.0f,
+         0.0f,  0.0f,  1.0f
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
