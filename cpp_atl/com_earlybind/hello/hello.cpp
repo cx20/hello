27a9805824a29863be887f958b83ca9091cd5b9a
cx20 <y-shimano@hondarer-soft.com>
2022-02-10 12:11:49 +0900

    Added COM sample in C++(ATL)

---
+++ cpp_atl/com_earlybind/hello/hello.cpp
@@ -0,0 +1,23 @@
+#include <atlbase.h>
+#include <shlobj.h>
+
+int _tmain(int argc, TCHAR* argv[] )
+{
+    CoInitialize( NULL );
+
+    CComPtr<Folder> pFolder;
+    CComPtr<IShellDispatch> pShell;
+    pShell.CoCreateInstance( CLSID_Shell );
+
+    CComVariant vRootFolder( ssfWINDOWS );
+    pShell->BrowseForFolder( 0, L"Hello, COM(ATL) World!", 0, vRootFolder, &pFolder );
+    if ( pFolder != NULL )
+    {
+        pFolder = NULL;
+    }
+    pShell = NULL;
+
+    CoUninitialize();
+
+    return 0;
+}
