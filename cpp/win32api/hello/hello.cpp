69caab40bde88f82c4f95768a25a8ca0698c4127
cx20 <y-shimano@hondarer-soft.com>
2022-02-11 00:16:30 +0900

    Added Win32 API sample in C++

---
+++ cpp/win32api/hello/hello.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain( int argc, TCHAR* argv[] )
+{
+    MessageBox( NULL, _T("Hello, Win32 API(C++) World!"), _T("Hello, World!"), MB_OK );
+    return 0;
+}
