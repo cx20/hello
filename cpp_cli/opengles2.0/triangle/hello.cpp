45b289a73b68c1379140a97a2ac827feb2979d0e
cx20 <y-shimano@hondarer-soft.com>
2022-02-16 00:13:07 +0900

    Added OpenGL ES 2.0/3.0 triangle samples in C++/CLI

---
+++ cpp_cli/opengles2.0/triangle/hello.cpp
@@ -0,0 +1,316 @@
+#include <windows.h>
+#include <tchar.h>
+#include <gl/gl.h>
+
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+
+#ifndef APIENTRYP
+#define APIENTRYP APIENTRY *
+#endif
+
+#define GL_ARRAY_BUFFER                   0x8892
+#define GL_STATIC_DRAW                    0x88E4
+#define GL_FRAGMENT_SHADER                0x8B30
+#define GL_VERTEX_SHADER                  0x8B31
+#define GL_SHADING_LANGUAGE_VERSION       0x8B8C
+
+#define WGL_CONTEXT_MAJOR_VERSION_ARB     0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB     0x2092
+#define WGL_CONTEXT_FLAGS_ARB             0x2094
+#define WGL_CONTEXT_PROFILE_MASK_ARB      0x9126
+#define WGL_CONTEXT_ES2_PROFILE_BIT_EXT   0x00000004
+
+typedef ptrdiff_t GLsizeiptr;
+typedef char GLchar;
+
+typedef void (APIENTRYP PFNGLGENBUFFERSPROC) (GLsizei n, GLuint *buffers);
+typedef void (APIENTRYP PFNGLBINDBUFFERPROC) (GLenum target, GLuint buffer);
+typedef void (APIENTRYP PFNGLBUFFERDATAPROC) (GLenum target, GLsizeiptr size, const void *data, GLenum usage);
+typedef GLuint (APIENTRYP PFNGLCREATESHADERPROC) (GLenum type);
+typedef void (APIENTRYP PFNGLSHADERSOURCEPROC) (GLuint shader, GLsizei count, const GLchar* const* string, const GLint* length);
+typedef void (APIENTRYP PFNGLCOMPILESHADERPROC) (GLuint shader);
+typedef GLuint (APIENTRYP PFNGLCREATEPROGRAMPROC) (void);
+typedef void (APIENTRYP PFNGLATTACHSHADERPROC) (GLuint program, GLuint shader);
+typedef void (APIENTRYP PFNGLLINKPROGRAMPROC) (GLuint program);
+typedef void (APIENTRYP PFNGLUSEPROGRAMPROC) (GLuint program);
+typedef GLint (APIENTRYP PFNGLGETATTRIBLOCATIONPROC) (GLuint program, const GLchar *name);
+typedef void (APIENTRYP PFNGLENABLEVERTEXATTRIBARRAYPROC) (GLuint index);
+typedef void (APIENTRYP PFNGLVERTEXATTRIBPOINTERPROC) (GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer);
+
+typedef HGLRC (WINAPI * PFNWGLCREATECONTEXTATTRIBSARBPROC) (HDC hDC, HGLRC hShareContext, const int *attribList);
+
+PFNGLGENBUFFERSPROC               glGenBuffers;
+PFNGLBINDBUFFERPROC               glBindBuffer;
+PFNGLBUFFERDATAPROC               glBufferData;
+PFNGLCREATESHADERPROC             glCreateShader;
+PFNGLSHADERSOURCEPROC             glShaderSource;
+PFNGLCOMPILESHADERPROC            glCompileShader;
+PFNGLCREATEPROGRAMPROC            glCreateProgram;
+PFNGLATTACHSHADERPROC             glAttachShader;
+PFNGLLINKPROGRAMPROC              glLinkProgram;
+PFNGLUSEPROGRAMPROC               glUseProgram;
+PFNGLGETATTRIBLOCATIONPROC        glGetAttribLocation;
+PFNGLENABLEVERTEXATTRIBARRAYPROC  glEnableVertexAttribArray;
+PFNGLVERTEXATTRIBPOINTERPROC      glVertexAttribPointer;
+
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HGLRC EnableOpenGL(HDC hDC);
+void DisableOpenGL(HWND hWnd, HDC hDC, HGLRC hRC);
+void InitOpenGLFunc();
+void InitShader();
+void DrawTriangle();
+
+// Shader sources
+const GLchar* vertexSource =
+    "attribute vec3 position;                     \n"
+    "attribute vec3 color;                        \n"
+    "varying   vec4 vColor;                       \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  vColor = vec4(color, 1.0);                 \n"
+    "  gl_Position = vec4(position, 1.0);         \n"
+    "}                                            \n";
+const GLchar* fragmentSource =
+    "precision mediump float;                     \n"
+    "varying   vec4 vColor;                       \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  gl_FragColor = vColor;                     \n"
+    "}                                            \n";
+
+GLuint vbo[2];
+GLint posAttrib;
+GLint colAttrib;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wcex;
+    HWND hWnd;
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
+    BOOL bQuit = FALSE;
+
+    wcex.cbSize         = sizeof(WNDCLASSEX);
+    wcex.style          = CS_OWNDC;
+    wcex.lpfnWndProc    = WindowProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = _T("WindowClass");
+    wcex.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    hWnd = CreateWindowEx(0,
+                          _T("WindowClass"),
+                          _T("Hello, World!"),
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          640,
+                          480,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+    hDC = GetDC(hWnd);
+    hRC = EnableOpenGL(hDC);
+    
+    InitOpenGLFunc();
+
+    InitShader();
+
+    while (!bQuit)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            DrawTriangle();
+
+            SwapBuffers(hDC);
+
+            Sleep (1);
+        }
+    }
+
+    DisableOpenGL(hWnd, hDC, hRC);
+    DestroyWindow(hWnd);
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+        break;
+
+        case WM_DESTROY:
+            return 0;
+
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+void InitOpenGLFunc()
+{
+    glGenBuffers              = (PFNGLGENBUFFERSPROC)              wglGetProcAddress("glGenBuffers");
+    glBindBuffer              = (PFNGLBINDBUFFERPROC)              wglGetProcAddress("glBindBuffer");
+    glBufferData              = (PFNGLBUFFERDATAPROC)              wglGetProcAddress("glBufferData");
+    glCreateShader            = (PFNGLCREATESHADERPROC)            wglGetProcAddress("glCreateShader");
+    glShaderSource            = (PFNGLSHADERSOURCEPROC)            wglGetProcAddress("glShaderSource");
+    glCompileShader           = (PFNGLCOMPILESHADERPROC)           wglGetProcAddress("glCompileShader");
+    glCreateProgram           = (PFNGLCREATEPROGRAMPROC)           wglGetProcAddress("glCreateProgram");
+    glAttachShader            = (PFNGLATTACHSHADERPROC)            wglGetProcAddress("glAttachShader");
+    glLinkProgram             = (PFNGLLINKPROGRAMPROC)             wglGetProcAddress("glLinkProgram");
+    glUseProgram              = (PFNGLUSEPROGRAMPROC)              wglGetProcAddress("glUseProgram");
+    glGetAttribLocation       = (PFNGLGETATTRIBLOCATIONPROC)       wglGetProcAddress("glGetAttribLocation");
+    glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC) wglGetProcAddress("glEnableVertexAttribArray");
+    glVertexAttribPointer     = (PFNGLVERTEXATTRIBPOINTERPROC)     wglGetProcAddress("glVertexAttribPointer");
+
+    wglCreateContextAttribsARB= (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+}
+
+HGLRC EnableOpenGL(HDC hDC)
+{
+    HGLRC hRC = NULL;
+    
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(hDC, &pfd);
+
+    SetPixelFormat(hDC, iFormat, &pfd);
+
+    HGLRC hGLRC_old = wglCreateContext(hDC);
+    wglMakeCurrent(hDC, hGLRC_old);
+
+    wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+
+    static const int  opengl_es20[]= {
+      WGL_CONTEXT_MAJOR_VERSION_ARB,   2,
+      WGL_CONTEXT_MINOR_VERSION_ARB,   0,
+      WGL_CONTEXT_FLAGS_ARB,           0,
+      WGL_CONTEXT_PROFILE_MASK_ARB,    WGL_CONTEXT_ES2_PROFILE_BIT_EXT,
+      0,
+    };
+    hRC = wglCreateContextAttribsARB(hDC, 0, opengl_es20 );
+
+    wglMakeCurrent(hDC, hRC);
+    wglDeleteContext(hGLRC_old);
+
+    const GLubyte * strShaderLanguageVersion = glGetString (GL_SHADING_LANGUAGE_VERSION);
+    
+    return hRC;
+}
+
+void DisableOpenGL(HWND hWnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(hRC);
+    ReleaseDC(hWnd, hDC);
+}
+
+void InitShader()
+{
+    glGenBuffers(2, vbo);
+
+    GLfloat vertices[] = {
+          0.0f,  0.5f, 0.0f, 
+          0.5f, -0.5f, 0.0f, 
+         -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] = {
+         1.0f,  0.0f,  0.0f,
+         0.0f,  1.0f,  0.0f,
+         0.0f,  0.0f,  1.0f
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+
+    colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+}
+
+void DrawTriangle()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw a triangle from the 3 vertices
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
