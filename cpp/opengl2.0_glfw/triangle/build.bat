a47503a6dd6b6882adf9ae3c6a3afc6b59603d00
cx20 <y-shimano@hondarer-soft.com>
2022-02-20 18:21:02 +0900

    Added OpenGL 1.0/1.1/2.0 triangle samples in C++(+GLFW)

---
+++ cpp/opengl2.0_glfw/triangle/build.bat
@@ -0,0 +1,15 @@
+SET GLEW_HOME=C:\Libraries\glew-2.1.0
+SET GLFW_HOME=C:\Libraries\glfw-3.3.6.bin.WIN64
+SET INCLUDE=%GLEW_HOME%\include;%GLFW_HOME%\include;%INCLUDE%
+SET LIB=%GLEW_HOME%\lib\Release\x64;%GLFW_HOME%\lib-vc2022;%LIB%
+
+cl hello.cpp ^
+         /link ^
+         user32.lib ^
+         shell32.lib ^
+         gdi32.lib ^
+         opengl32.lib ^
+         glu32.lib ^
+         glew32s.lib ^
+         glfw3_mt.lib ^
+         /SUBSYSTEM:WINDOWS
