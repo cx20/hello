275596b1b36821b3c50f22b723aa076c535ee09e
cx20 <y-shimano@hondarer-soft.com>
2022-02-24 01:01:22 +0900

    Added OpenGL ES 2.0/3.0  triangle samples in Java(+LWJGL)

---
+++ java/opengles2.0_lwjgl/triangle/run.bat
@@ -0,0 +1,2 @@
+SET LWJGL_CLASSPATH=lwjgl.jar;lwjgl-opengl.jar;lwjgl-opengles.jar;lwjgl-glfw.jar;lwjgl-natives-windows.jar;lwjgl-opengl-natives-windows.jar;lwjgl-opengles-natives-windows.jar
+java -cp %LWJGL_CLASSPATH%;. Hello
