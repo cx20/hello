771e2e1958db5ed5c58ccaee882fd1dd3d6f31c7
cx20 <y-shimano@hondarer-soft.com>
2022-02-27 23:23:53 +0900

    Added DirectX9 triangle sample in C#(+SharpDX)

---
+++ csharp/directx9_sharpdx/triangle/build.bat
@@ -0,0 +1,1 @@
+dotnet build Hello.csproj /p:Configuration=Release
---
+++ csharp/directx9_sharpdx/triangle/publish.bat
@@ -0,0 +1,5 @@
+copy bin\x64\Release\net48-windows\win-x64\Hello.exe
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Direct3D9.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Desktop.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Mathematics.dll
