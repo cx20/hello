3edb3dee9c3065105ab25d5cade91b903a0f6cc8
cx20 <y-shimano@hondarer-soft.com>
2022-02-28 01:23:39 +0900

    Added DirectX11 triangle sample in C++(ATL)

---
+++ cpp_atl/directx11/triangle/build.bat
@@ -0,0 +1,9 @@
+SET DXSDK_DIR=C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)
+SET INCLUDE=%INCLUDE%;%DXSDK_DIR%\INCLUDE
+SET LIB=%LIB%;%DXSDK_DIR%\Lib\x86
+
+cl hello.cpp ^
+         /link ^
+         dxguid.lib ^
+         d3d11.lib ^
+         d3dx11.lib
