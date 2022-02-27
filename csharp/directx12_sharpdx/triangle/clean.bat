9baa5056e59cdd90425c2c447b2fa85163e12d87
cx20 <y-shimano@hondarer-soft.com>
2022-02-28 00:22:45 +0900

    Added DirectX12 triangle sample in C#(+SharpDX)

---
+++ csharp/directx12_sharpdx/triangle/clean.bat
@@ -0,0 +1,7 @@
+del Hello.exe
+del SharpDX.dll
+del SharpDX.DXGI.dll
+del SharpDX.D3DCompiler.dll
+del SharpDX.Desktop.dll
+del SharpDX.Direct3D12.dll
+del SharpDX.Mathematics.dll
---
+++ csharp/directx12_sharpdx/triangle/publish.bat
@@ -0,0 +1,7 @@
+copy bin\x64\Release\net48-windows\win-x64\Hello.exe
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.DXGI.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.D3DCompiler.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Desktop.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Direct3D12.dll
+copy bin\x64\Release\net48-windows\win-x64\SharpDX.Mathematics.dll
