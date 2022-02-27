46745f1eacef24b65c16152aea084fb214284f76
cx20 <y-shimano@hondarer-soft.com>
2022-02-27 16:41:13 +0900

    Added DirectX11 triangle sample in C#(+SharpDX)

---
+++ csharp/directx11_sharpdx/triangle/publish.bat
@@ -0,0 +1,10 @@
+dotnet publish /p:Configuration=Release
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.Mathematics.dll
+copy bin\x64\Release\net6.0-windows\win-x64\Hello.dll
+copy bin\x64\Release\net6.0-windows\win-x64\Hello.exe
+copy bin\x64\Release\net6.0-windows\win-x64\Hello.runtimeconfig.json
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.D3DCompiler.dll
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.Direct2D1.dll
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.Direct3D11.dll
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.dll
+copy bin\x64\Release\net6.0-windows\win-x64\SharpDX.DXGI.dll
