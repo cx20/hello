f2752b41db46dd5aff7954d219648d63561b1205
cx20 <y-shimano@hondarer-soft.com>
2022-03-01 02:33:26 +0900

    Added OpenGL ES 2.0/3.0 triangle sample in C++(ATL)

---
+++ cpp_atl/opengles2.0/triangle/hello.cpp
@@ -0,0 +1,282 @@
+#include <atlbase.h>
+#include <atlwin.h>
+#include <gl/gl.h>
+ 
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+
+#ifndef APIENTRYP
+#define APIENTRYP APIENTRY *
+#endif
+
+#define GL_ARRAY_BUFFER                   0x8892
+#define GL_STATIC_DRAW                    0x88E4
+#define GL_FRAGMENT_SHADER                0x8B30
+#define GL_VERTEX_SHADER                  0x8B31
+
+#define WGL_CONTEXT_MAJOR_VERSION_ARB     0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB     0x2092
+#define WGL_CONTEXT_FLAGS_ARB             0x2094
+#define WGL_CONTEXT_PROFILE_MASK_ARB      0x9126
+#define WGL_CONTEXT_ES2_PROFILE_BIT_EXT   0x00000004
+
+typedef ptrdiff_t GLsizeiptr;
+typedef char GLchar;
+
+typedef void (APIENTRYP PFNGLGENBUFFERSPROC) (GLsizei n, GLuint *buffers);
+typedef void (APIENTRYP PFNGLBINDBUFFERPROC) (GLenum target, GLuint buffer);
+typedef void (APIENTRYP PFNGLBUFFERDATAPROC) (GLenum target, GLsizeiptr size, const void *data, GLenum usage);
+typedef GLuint (APIENTRYP PFNGLCREATESHADERPROC) (GLenum type);
+typedef void (APIENTRYP PFNGLSHADERSOURCEPROC) (GLuint shader, GLsizei count, const GLchar* const* string, const GLint* length);
+typedef void (APIENTRYP PFNGLCOMPILESHADERPROC) (GLuint shader);
+typedef GLuint (APIENTRYP PFNGLCREATEPROGRAMPROC) (void);
+typedef void (APIENTRYP PFNGLATTACHSHADERPROC) (GLuint program, GLuint shader);
+typedef void (APIENTRYP PFNGLLINKPROGRAMPROC) (GLuint program);
+typedef void (APIENTRYP PFNGLUSEPROGRAMPROC) (GLuint program);
+typedef GLint (APIENTRYP PFNGLGETATTRIBLOCATIONPROC) (GLuint program, const GLchar *name);
+typedef void (APIENTRYP PFNGLENABLEVERTEXATTRIBARRAYPROC) (GLuint index);
+typedef void (APIENTRYP PFNGLVERTEXATTRIBPOINTERPROC) (GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer);
+typedef HGLRC (WINAPI * PFNWGLCREATECONTEXTATTRIBSARBPROC) (HDC hDC, HGLRC hShareContext, const int *attribList);
+
+PFNGLGENBUFFERSPROC               glGenBuffers;
+PFNGLBINDBUFFERPROC               glBindBuffer;
+PFNGLBUFFERDATAPROC               glBufferData;
+PFNGLCREATESHADERPROC             glCreateShader;
+PFNGLSHADERSOURCEPROC             glShaderSource;
+PFNGLCOMPILESHADERPROC            glCompileShader;
+PFNGLCREATEPROGRAMPROC            glCreateProgram;
+PFNGLATTACHSHADERPROC             glAttachShader;
+PFNGLLINKPROGRAMPROC              glLinkProgram;
+PFNGLUSEPROGRAMPROC               glUseProgram;
+PFNGLGETATTRIBLOCATIONPROC        glGetAttribLocation;
+PFNGLENABLEVERTEXATTRIBARRAYPROC  glEnableVertexAttribArray;
+PFNGLVERTEXATTRIBPOINTERPROC      glVertexAttribPointer;
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
+
+// Shader sources
+const GLchar* vertexSource =
+    "attribute vec3 position;                     \n"
+    "attribute vec3 color;                        \n"
+    "varying   vec4 vColor;                       \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  vColor = vec4(color, 1.0);                 \n"
+    "  gl_Position = vec4(position, 1.0);         \n"
+    "}                                            \n";
+const GLchar* fragmentSource =
+    "precision mediump float;                     \n"
+    "varying vec4 vColor;                         \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  gl_FragColor = vColor;                     \n"
+    "}                                            \n";
+
+class CHelloWindow : public CWindowImpl<CHelloWindow>
+{
+    BEGIN_MSG_MAP( CHelloWindow )
+        MESSAGE_HANDLER( WM_CREATE,  OnCreate  )
+        MESSAGE_HANDLER( WM_PAINT,   OnPaint   )
+        MESSAGE_HANDLER( WM_DESTROY, OnDestroy )
+    END_MSG_MAP()
+
+    LRESULT OnCreate( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+    LRESULT OnPaint( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+    LRESULT OnDestroy( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+    
+public:
+    void EnableOpenGL();
+    void DisableOpenGL();
+    void InitOpenGLFunc();
+    void InitShader();
+    void DrawTriangle();
+
+private:
+    HDC   m_hDC;
+    HGLRC m_hRC;
+    GLuint m_vbo[2];
+    GLint m_posAttrib;
+    GLint m_colAttrib;
+
+};
+
+
+LRESULT CHelloWindow::OnCreate( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled )
+{
+    EnableOpenGL();
+
+    InitOpenGLFunc();
+
+    InitShader();
+    
+    ResizeClient( 640, 480 );
+    glViewport( 0, 0, 640, 480 );
+    
+    return 0L;
+}
+
+LRESULT CHelloWindow::OnPaint( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled )
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    DrawTriangle();
+
+    SwapBuffers(m_hDC);
+    
+    return 0;
+}
+
+LRESULT CHelloWindow::OnDestroy( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled )
+{
+    PostQuitMessage( 0 );
+
+    DisableOpenGL();
+
+    return 0;
+}
+
+void CHelloWindow::EnableOpenGL()
+{
+    m_hDC = GetDC();
+    
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(m_hDC, &pfd);
+
+    SetPixelFormat(m_hDC, iFormat, &pfd);
+    HGLRC hGLRC_old = wglCreateContext(m_hDC);
+    wglMakeCurrent(m_hDC, hGLRC_old);
+
+    wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+
+    static const int  opengl_es20[]= {
+      WGL_CONTEXT_MAJOR_VERSION_ARB,   2,
+      WGL_CONTEXT_MINOR_VERSION_ARB,   0,
+      WGL_CONTEXT_FLAGS_ARB,           0,
+      WGL_CONTEXT_PROFILE_MASK_ARB,    WGL_CONTEXT_ES2_PROFILE_BIT_EXT,
+      0,
+    };
+    m_hRC = wglCreateContextAttribsARB(m_hDC, 0, opengl_es20);
+
+    wglMakeCurrent(m_hDC, m_hRC);
+    wglDeleteContext(hGLRC_old);
+}
+
+void CHelloWindow::DisableOpenGL()
+{
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(m_hRC);
+    ReleaseDC(m_hDC);
+}
+
+void CHelloWindow::InitOpenGLFunc()
+{
+    glGenBuffers              = (PFNGLGENBUFFERSPROC)              wglGetProcAddress("glGenBuffers");
+    glBindBuffer              = (PFNGLBINDBUFFERPROC)              wglGetProcAddress("glBindBuffer");
+    glBufferData              = (PFNGLBUFFERDATAPROC)              wglGetProcAddress("glBufferData");
+    glCreateShader            = (PFNGLCREATESHADERPROC)            wglGetProcAddress("glCreateShader");
+    glShaderSource            = (PFNGLSHADERSOURCEPROC)            wglGetProcAddress("glShaderSource");
+    glCompileShader           = (PFNGLCOMPILESHADERPROC)           wglGetProcAddress("glCompileShader");
+    glCreateProgram           = (PFNGLCREATEPROGRAMPROC)           wglGetProcAddress("glCreateProgram");
+    glAttachShader            = (PFNGLATTACHSHADERPROC)            wglGetProcAddress("glAttachShader");
+    glLinkProgram             = (PFNGLLINKPROGRAMPROC)             wglGetProcAddress("glLinkProgram");
+    glUseProgram              = (PFNGLUSEPROGRAMPROC)              wglGetProcAddress("glUseProgram");
+    glGetAttribLocation       = (PFNGLGETATTRIBLOCATIONPROC)       wglGetProcAddress("glGetAttribLocation");
+    glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC) wglGetProcAddress("glEnableVertexAttribArray");
+    glVertexAttribPointer     = (PFNGLVERTEXATTRIBPOINTERPROC)     wglGetProcAddress("glVertexAttribPointer");
+    wglCreateContextAttribsARB= (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+}
+
+void CHelloWindow::InitShader()
+{
+    glGenBuffers(2, m_vbo);
+
+    GLfloat vertices[] = {
+          0.0f,  0.5f, 0.0f, 
+          0.5f, -0.5f, 0.0f, 
+         -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] = {
+         1.0f,  0.0f,  0.0f,
+         0.0f,  1.0f,  0.0f,
+         0.0f,  0.0f,  1.0f
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    m_posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(m_posAttrib);
+
+    m_colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(m_colAttrib);
+}
+
+void CHelloWindow::DrawTriangle()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[0]);
+    glVertexAttribPointer(m_posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[1]);
+    glVertexAttribPointer(m_colAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw a triangle from the 3 vertices
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+CComModule _Module;
+
+int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
+{
+    _Module.Init(NULL, hInstance);
+ 
+    CHelloWindow wnd;
+    wnd.Create( NULL, CWindow::rcDefault, _T("Hello, World!"), WS_OVERLAPPEDWINDOW | WS_VISIBLE );
+    wnd.ResizeClient( 640, 480 );
+    MSG msg;
+    while( GetMessage( &msg, NULL, 0, 0 ) ){
+        TranslateMessage( &msg );
+        DispatchMessage( &msg );
+    }
+ 
+    _Module.Term();
+ 
+    return msg.wParam;
+}
