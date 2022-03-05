8d1e16c965a1d70dfe77a22dfbdc337626b40655
cx20 <y-shimano@hondarer-soft.com>
2022-03-06 01:20:50 +0900

    Improved to be able to build with multiple architectures

--- cpp_cli/directx10/triangle/build.bat
+++ cpp_cli/directx10/triangle/build.bat
@@ -1,6 +1,6 @@
 SET DXSDK_DIR=C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)
 SET INCLUDE=%INCLUDE%;%DXSDK_DIR%\INCLUDE;
-SET LIB=%LIB%;%DXSDK_DIR%\Lib\x86;
+SET LIB=%LIB%;%DXSDK_DIR%\Lib\%VSCMD_ARG_TGT_ARCH%;
 
 cl hello.cpp ^
          /clr ^
