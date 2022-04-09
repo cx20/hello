efb5b2871a00f2337432687442adbb7c568e75d1
cx20 <y-shimano@hondarer-soft.com>
2022-04-09 15:45:23 +0900

    Added Vulkan 1.3  triangle sample in C++

---
+++ cpp/vulkan1.3/triangle/build.bat
@@ -0,0 +1,17 @@
+SET VULKAN_SDK=C:\VulkanSDK\1.3.204.0
+SET INCLUDE=%INCLUDE%;%VULKAN_SDK%\Include;C:\Libraries\glfw-3.3.6.bin.WIN64\include;C:\Libraries\glm
+SET LIB=%LIB%;%VULKAN_SDK%\Lib;C:\Libraries\glfw-3.3.6.bin.WIN64\lib-vc2022
+SET PATH=%PATH%;%VULKAN_SDK%\bin
+
+glslc.exe hello.vert -o hello_vert.spv
+glslc.exe hello.frag -o hello_frag.spv
+
+cl hello.cpp ^
+         /MD ^
+         /std:c++17 ^
+         /link ^
+         user32.lib ^
+         gdi32.lib ^
+         shell32.lib ^
+         vulkan-1.lib ^
+         glfw3.lib
