cd8a22848e55e6d2e122b611a53517f70c4b3f44
cx20 <y-shimano@hondarer-soft.com>
2023-01-15 00:25:59 +0900

    Added helper batch for build

---
+++ cpp/webgpu_glfw/triangle/clean.bat
@@ -0,0 +1,10 @@
+del glfw3webgpu.c
+del glfw3webgpu.h
+del glfw3webgpu.obj
+del hello.exe
+del hello.obj
+del webgpu.h
+del webgpu.hpp
+del wgpu.h
+del wgpu_native.dll
+del wgpu_native.lib
---
+++ cpp/webgpu_glfw/triangle/install.bat
@@ -0,0 +1,7 @@
+curl -o glfw3webgpu.c   https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/glfw3webgpu/glfw3webgpu.c
+curl -o glfw3webgpu.h   https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/glfw3webgpu/glfw3webgpu.h
+curl -o webgpu.h        https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/webgpu/webgpu.h
+curl -o webgpu.hpp      https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/webgpu/webgpu.hpp
+curl -o wgpu.h          https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/webgpu/wgpu.h
+curl -o wgpu_native.dll https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/webgpu/windows-x86_64/wgpu_native.dll
+curl -o wgpu_native.lib https://raw.githubusercontent.com/eliemichel/LearnWebGPU-Code/step030/webgpu/windows-x86_64/wgpu_native.lib
