e7b3c1105f61e318be2be69d86b14caa6691b703
cx20 <y-shimano@hondarer-soft.com>
2024-08-18 02:20:13 +0900

    Added DirectX9 triangle samples in F#

---
+++ fsharp/directx9/triangle/build.bat
@@ -0,0 +1,7 @@
+fsc ^
+   --platform:x86 ^
+   -r:System.Windows.Forms.dll ^
+   -r:"C:\Windows\assembly\GAC\Microsoft.DirectX\1.0.2902.0__31bf3856ad364e35\Microsoft.DirectX.dll" ^
+   -r:"C:\Windows\assembly\GAC\Microsoft.DirectX.Direct3D\1.0.2902.0__31bf3856ad364e35\Microsoft.DirectX.Direct3D.dll" ^
+   -r:"C:\Windows\assembly\GAC_MSIL\Microsoft.VisualC\8.0.0.0__b03f5f7f11d50a3a\Microsoft.VisualC.Dll" ^
+   Hello.fs
