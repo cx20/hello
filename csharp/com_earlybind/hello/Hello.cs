6c5869d238e363715b7dadf2f8c511b4b103d480
cx20 <yshimano@gmail.com>
2026-01-25 02:04:58 +0900

    Improved C# COM earlybind sample

--- csharp/com_earlybind/hello/Hello.cs
+++ csharp/com_earlybind/hello/Hello.cs
@@ -6,11 +6,18 @@ class Hello
 {
     static void Main(String[] args)
     {
-        Shell shell = new Shell();
-        Object vRootFolder = (long)ShellSpecialFolderConstants.ssfWINDOWS;
+        // NOTE: Avoid `new Shell()` (tlbimp may default to IShellDispatch6, 
+        // which may be unsupported); use ProgID + IShellDispatch for compatibility.
+
+        Type shellType = Type.GetTypeFromProgID("Shell.Application");
+        IShellDispatch shell = (IShellDispatch)Activator.CreateInstance(shellType);
+
+        object vRootFolder = (int)ShellSpecialFolderConstants.ssfWINDOWS;
         Folder folder = shell.BrowseForFolder(0, "Hello, COM(C#) World!", 0, vRootFolder);
+
         if (folder != null)
         {
+            Console.WriteLine("Selected: " + folder.Title);
             Marshal.ReleaseComObject(folder);
         }
         Marshal.ReleaseComObject(shell);
