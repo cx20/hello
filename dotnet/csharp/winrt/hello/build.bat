5255553a65c47ee0053042cc4f0f6ab14c9ce6cf
cx20 <yshimano@gmail.com>
2026-02-21 21:15:53 +0900

    Added C# WinRT Hello sample

---
+++ dotnet/csharp/winrt/hello/build.bat
@@ -0,0 +1,4 @@
+csc /nologo /target:exe /platform:x64 ^
+  /r:"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.26100.0\Windows.winmd" ^
+  /r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Runtime.WindowsRuntime.dll" ^
+  Hello.cs
