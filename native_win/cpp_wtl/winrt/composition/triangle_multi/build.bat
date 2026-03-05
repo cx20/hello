94985bcf23f052a5e6cf4c48ed086db5c99859b8
cx20 <yshimano@gmail.com>
2026-03-06 00:10:53 +0900

    Added C++ (WTL) WinRT Composition + OpenGL + DirectX + Vulkan triangles sample

---
+++ native_win/cpp_wtl/winrt/composition/triangle_multi/build.bat
@@ -0,0 +1,24 @@
+SET VULKAN_SDK=C:\VulkanSDK\1.4.335.0
+SET INCLUDE=C:\WTL\WTL10_10320_Release\Include;%INCLUDE%
+SET INCLUDE=%INCLUDE%;%VULKAN_SDK%\Include
+SET LIB=%LIB%;%VULKAN_SDK%\Lib
+SET PATH=%PATH%;%VULKAN_SDK%\bin
+
+glslc.exe hello.vert -o hello_vert.spv
+glslc.exe hello.frag -o hello_frag.spv
+
+cl /EHsc ^
+   /std:c++17 ^
+   /I%VULKAN_SDK%\Include ^
+   hello.cpp ^
+   /link ^
+   /LIBPATH:%VULKAN_SDK%\Lib ^
+   vulkan-1.lib ^
+   d3d11.lib ^
+   dxgi.lib ^
+   d3dcompiler.lib ^
+   opengl32.lib ^
+   user32.lib ^
+   gdi32.lib ^
+   runtimeobject.lib ^
+   CoreMessaging.lib
