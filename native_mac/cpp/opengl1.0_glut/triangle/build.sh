feabeaf40868df798b796d956ba6ca10c096b128
cx20 <y-shimano@hondarer-soft.com>
2026-04-01 22:17:52 +0900

    Add macOS C++ OpenGL 1.0/1.1/2.0 GLUT triangle samples

---
+++ native_mac/cpp/opengl1.0_glut/triangle/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+c++ -DGL_SILENCE_DEPRECATION -std=c++17 -o hello hello.cpp -framework OpenGL -framework GLUT
---
+++ native_mac/cpp/opengl1.0_glut/triangle/hello.cpp
@@ -0,0 +1,62 @@
+#include <GLUT/glut.h>
+
+#include <cstdlib>
+
+void Display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f); glVertex2f( 0.0f,  0.7f);
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex2f(-0.7f, -0.7f);
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex2f( 0.7f, -0.7f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void Reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void Timer(int value)
+{
+    (void)value;
+    glutPostRedisplay();
+    glutTimerFunc(16, Timer, 0);
+}
+
+void OnClose()
+{
+    std::exit(0);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+    (void)x;
+    (void)y;
+
+    if (key == 27 || key == 'q' || key == 'Q') {
+        std::exit(0);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Hello, OpenGL 1.0 World!");
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    glutDisplayFunc(Display);
+    glutReshapeFunc(Reshape);
+    glutWMCloseFunc(OnClose);
+    glutKeyboardFunc(Keyboard);
+    glutTimerFunc(16, Timer, 0);
+    glutMainLoop();
+
+    return 0;
+}
